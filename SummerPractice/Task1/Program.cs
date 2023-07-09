941125bc9293049d2928861a07c02ae1c9b5ab95
swiftcr7 <dimkafedulov@yandex.ru>
2023-07-09 21:17:29 +0300

    Task1 added

---
+++ SummerPractice/Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student("Fedulov", "Dima", "A", "M8O-211B-21", Student.practice_course_mode.C);
+            Student student2 = new Student("Dush santush aveiru", "Cristiano Ronaldo", "dcm", "M8O-211B-21", Student.practice_course_mode.C);
+            Console.WriteLine(student1.Course);
+            Console.WriteLine(student1); 
+            Console.WriteLine(student2.Equals(student1));
+        }
+    }
+}
